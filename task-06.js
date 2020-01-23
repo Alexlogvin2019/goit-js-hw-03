82b82117a78a3b44d0405d3048a1d10162eadf9f
Alexlogvin2019 <alexlogvin@yahoo.com>
2020-01-23 10:13:21 +0200

    ver1

---
+++ task-06.js
@@ -0,0 +1,25 @@
+"use strict";
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Сканер", price: 2700, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 }
+];
+
+const calculateTotalPrice = function(allProdcuts, productName) {
+  let cost;
+  for (let i = 0; i < allProdcuts.length; i++) {
+    if (productName === allProdcuts[i].name) {
+      let value = allProdcuts[i].price * allProdcuts[i].quantity;
+      cost = value;
+    }
+  }
+  return cost;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(calculateTotalPrice(products, "Радар")); // 5200
+
+console.log(calculateTotalPrice(products, "Дроид")); // 2800
